a6fa0d882982f0af15d4f39fbf6ac998b3e96e88
miraclew <miraclew@gmail.com>
2020-06-24 17:11:25 +0800

    add more common

--- pkg/pb/empty.go
+++ pkg/pb/empty.go
@@ -14,3 +14,25 @@ type UserAvatar struct {
 	Nickname string
 	Avatar   string
 }
+
+type LimitOffset struct {
+	Offset int64
+	Limit  int32
+}
+
+type QueryRequest struct {
+	Offset int64
+	Limit  int32
+	Filter map[string]interface{}
+	Sort   string
+}
+
+type GetRequest struct {
+	Id     int64
+	Filter map[string]interface{}
+}
+
+type UpdateRequest struct {
+	Id     int64
+	Values map[string]interface{}
+}
--- pkg/tsql/tsql.go
+++ pkg/tsql/tsql.go
@@ -1,1 +1,54 @@
 package tsql
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type QueryParams struct {
+	Ids    []int64
+	Filter Filter
+	Sort   string
+	Offset int64
+	Limit  int32
+}
+
+type GetParams struct {
+	Id     int64
+	Filter Filter
+}
+
+type Values map[string]interface{}
+
+func (vs Values) UpdateSet() (string, []interface{}) {
+	var ss []string
+	var values []interface{}
+	for k, v := range vs {
+		// check increment in update statement, e.g. count=count+1
+		if s, ok := v.(string); ok && strings.HasPrefix(s, k) {
+			s = strings.TrimSpace(strings.TrimPrefix(s, k))
+			if len(s) >= 2 && (s[0:1] == "+" || s[0:1] == "-") {
+				ss = append(ss, fmt.Sprintf("%s=%s%s?", k, k, s[0:1]))
+				d, _ := strconv.ParseInt(strings.TrimSpace(s[1:]), 10, 64)
+				values = append(values, d)
+				continue
+			}
+		}
+		ss = append(ss, k+"=?")
+		values = append(values, v)
+	}
+	return strings.Join(ss, ","), values
+}
+
+type Filter map[string]interface{}
+
+func (f Filter) Conditions() (string, []interface{}) {
+	var cond []string
+	var values []interface{}
+	for k, v := range f {
+		cond = append(cond, k+"=?")
+		values = append(values, v)
+	}
+	return strings.Join(cond, " AND "), values
+}
